e8625ccce5197c875ccc6beaa2c4694055187ca0
Hamza-ye <7amza.it@gmail.com>
2021-04-12 01:45:46 +0300

    format video screen

--- lib/screens/video_screen.dart
+++ lib/screens/video_screen.dart
@@ -1,71 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_youtube_ui_clone/screens/nav_screen.dart';
-import 'package:timeago/timeago.dart' as timeago;
+import 'package:flutter_youtube_ui_clone/widgets/widgets.dart';
+import 'package:miniplayer/miniplayer.dart';
 
 class VideoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, watch, child) {
-        final selectedVideo = watch(selectedVideoProvider).state;
-        return Container(
-          color: Theme.of(context).scaffoldBackgroundColor,
-          child: Column(
-            children: [
-              Column(
-                children: [
-                  Image.network(
-                    selectedVideo!.thumbnailUrl,
-                    height: 220.0,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                  LinearProgressIndicator(
-                    value: 0.4,
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
-                  ),
-                ],
+    return Scaffold(
+      body: Container(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Consumer(
+                builder: (context, watch, child) {
+                  final selectedVideo = watch(selectedVideoProvider).state;
+                  return SafeArea(
+                    child: Column(
+                      children: [
+                        Stack(
+                          children: [
+                            Image.network(
+                              selectedVideo!.thumbnailUrl,
+                              height: 220.0,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                            Positioned(
+                              child: IconButton(
+                                iconSize: 30.0,
+                                icon: const Icon(Icons.keyboard_arrow_down),
+                                onPressed: () {
+                                  context
+                                      .read(miniPlayerControllerProvider)
+                                      .state
+                                      .animateToHeight(state: PanelState.MIN);
+                                },
+                              ),
+                              top: 8.0,
+                              left: 8.0,
+                            ),
+                          ],
+                        ),
+                        const LinearProgressIndicator(
+                          value: 0.4,
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                        ),
+                        VideoInfo(video: selectedVideo),
+                      ],
+                    ),
+                  );
+                },
               ),
-              Expanded(
-                child: ListView(
-                  padding: EdgeInsets.only(top: 8.0),
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            selectedVideo.title,
-                            overflow: TextOverflow.ellipsis,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .copyWith(fontSize: 15.0),
-                          ),
-                          SizedBox(
-                            height: 8.0,
-                          ),
-                          Text(
-                            '${selectedVideo.viewCount} views . ${timeago.format(selectedVideo.timestamp)}',
-                            overflow: TextOverflow.ellipsis,
-                            style: Theme.of(context)
-                                .textTheme
-                                .caption!
-                                .copyWith(fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        );
-      },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
